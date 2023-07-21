2e87a087319e579f8203f26cecb497c8a479f55d
Kelvin Chin <chinkaiwen@yahoo.com>
2023-07-21 19:28:39 +0800

    Create outer template of the site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'this-years-progress';
+export class AppComponent implements OnInit {
+  title = "This Year's Progress";
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
